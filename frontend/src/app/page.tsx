a5eab21b0990094ea582fa9a337439e110f56657
rabbitglauser <samuel.glauser@gmail.com>
2026-04-11 22:52:19 +0200

    [feature] Add Footer component to the login page with legal links

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,18 @@
 import LoginForm from "@/components/Login/LoginForm";
 import { AdminHero } from "@/components/Login/AdminHero";
+import { Footer } from "@/components/Login/footer";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen">
+    <main className="relative flex min-h-screen">
       <section className="hidden w-1/2 items-center justify-center lg:flex">
         <AdminHero />
       </section>
       <section className="flex w-full items-center justify-center px-6 py-10 lg:w-1/2 lg:px-10">
         <LoginForm />
       </section>
+
+        <Footer />
     </main>
   );
 }

c0e12f3207884ff328436af1fa561834b6cbfd17
rabbitglauser <samuel.glauser@gmail.com>
2026-04-11 22:43:57 +0200

    [feature] Restructure components and enhance login page with AdminHero section; remove unused assets

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,12 @@
-import LoginForm from "@/components/LoginForm";
+import LoginForm from "@/components/Login/LoginForm";
+import { AdminHero } from "@/components/Login/AdminHero";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen items-center justify-end">
+    <main className="flex min-h-screen">
+      <section className="hidden w-1/2 items-center justify-center lg:flex">
+        <AdminHero />
+      </section>
       <section className="flex w-full items-center justify-center px-6 py-10 lg:w-1/2 lg:px-10">
         <LoginForm />
       </section>
